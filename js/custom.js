a217e432cf503a1ab4649920453739ca92effa43
Arif Ahmod <arifahmod9c@gmail.com>
2022-02-23 22:40:37 +0600

    carousel design

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const userInfo = [
+    {
+        name: "Arif Ahmod",
+        occupation: "Front End Developer",
+        img: "https://cdn.pixabay.com/photo/2020/08/22/21/55/computer-5509452_1280.png",
+        text: "This is Arif Ahmod Riyad. He is approximately 18 years old. He is not Only Student but also a Front End Developer. He is from Bangladesh "
+    },
+
+];
+
+
+
+
+
