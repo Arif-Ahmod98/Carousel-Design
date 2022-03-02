0f0854ca5394022c5d5dd5b4c2897e6e312c5190
Arif Ahmod <arifahmod9c@gmail.com>
2022-03-02 16:42:05 +0600

    designed carousel with vanilla javascript

--- js/custom.js
+++ js/custom.js
@@ -2,15 +2,100 @@
 
 const userInfo = [
     {
+        name: "Peter Jones",
+        occupation: "Intern",
+        img: "images/22.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni recusandae inventore nisi culpa eos tenetur optio maiores voluptatibus!",
+    },
+    {
+        name: "Sabbir Ahmed",
+        occupation: "SEO Expert",
+        img: "images/sabbir.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed facilis error omnis eveniet a nulla eligendi. Digital Marketer"
+    },
+    {
         name: "Arif Ahmod",
         occupation: "Front End Developer",
-        img: "https://cdn.pixabay.com/photo/2020/08/22/21/55/computer-5509452_1280.png",
-        text: "This is Arif Ahmod Riyad. He is approximately 18 years old. He is not Only Student but also a Front End Developer. He is from Bangladesh "
+        img: "images/airfahmod.png",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam expedita quibusdam aspernatur animi sint cupiditate?. "
     },
+
+    {
+        name: "Mr. Janson",
+        occupation: "Digital Marketer",
+        img: "images/11.jpg",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, fugiat expedita ab aliquam perspiciatis hic repudiandae. Cupiditate, nobis? "
+    }
 
 ];
 
+const profile = document.getElementById("profile");
+const name = document.getElementById("name");
+const occupation = document.getElementById("occupation");
+const identity = document.getElementById("identity");
+const random = document.querySelector(".random-btn");
+const leftBtn = document.querySelector(".left-arrow");
+const rightBtn = document.querySelector(".right-arrow");
+let currentItem = 0;
+
+const showPerson = () => {
+    let item = userInfo[currentItem];
+    profile.src = item.img;
+    name.textContent = item.name;
+    occupation.textContent = item.occupation;
+    identity.textContent = item.text;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    showPerson();
+
+});
 
 
+// for next profile
+
+rightBtn.addEventListener('click', () => {
+
+    currentItem++;
+
+    if (currentItem > userInfo.length - 1) {
+
+        currentItem = 0;
+
+    }
+    else {
+
+        showPerson();
+
+    }
+
+});
 
 
+// for preview profile
+
+leftBtn.addEventListener('click', () => {
+
+    currentItem--;
+
+    if (currentItem < 0) {
+
+        currentItem = userInfo.length - 1;
+
+    }
+    else {
+
+        showPerson();
+
+    }
+
+});
+
+
+// for Random profile
+
+random.addEventListener("click", () => {
+    currentItem = Math.floor(Math.random() * userInfo.length);
+    showPerson();
+})
